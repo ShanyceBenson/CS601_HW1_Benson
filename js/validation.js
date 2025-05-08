d2a5719dbb085dafd6ca39bbedf9320e3707b4b9
Shanyce Benson <shanyce.benson@outlook.com>
2025-05-08 16:00:00 +0900

    summary added and form hidden on submission

--- js/validation.js
+++ js/validation.js
@@ -1,29 +1,29 @@
-document.getElementById("form").addEventListener("submit", function(event){
+document.getElementById("form").addEventListener("submit", function (event) {
     const firstName = document.getElementById("fname").value.trim();
     const lastName = document.getElementById("lname").value.trim();
     const emailVal = document.getElementById("email").value.trim();
     const packageSelect = document.getElementById("package");
     const sub = document.getElementById("subscribe");
-    
+
     const fnErr = document.getElementById("ferror");
     const lnErr = document.getElementById("lerror");
     const emErr = document.getElementById("emerror");
     const pErr = document.getElementById("perror");
     const subErr = document.getElementById("suberror");
-    
+
     const nameCheck = /^[A-Za-z]{2,}$/;
     const emailCheck = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    
+
     let isValid = true;
-    
-    if(!nameCheck.test(firstName)) {
+
+    if (!nameCheck.test(firstName)) {
         fnErr.textContent = "First name must have at least 2 letters A-Z only.";
         isValid = false;
     } else {
         fnErr.textContent = "";
     }
 
-    if(!nameCheck.test(lastName)) {
+    if (!nameCheck.test(lastName)) {
         lnErr.textContent = "Last name must have at least 2 letters A-Z only.";
         isValid = false;
     } else {
@@ -53,6 +53,16 @@ document.getElementById("form").addEventListener("submit", function(event){
 
     if (!isValid) {
         event.preventDefault();
+    } else {
+        event.preventDefault();
+        const summaryDiv = document.getElementById("summary");
+
+        summaryDiv.innerHTML = `Thank you, ${firstName} ${lastName}, for subscribing!
+                                Your email <strong>${emailVal}</strong> is registered 
+                                with our <strong>${packageSelect.value}</strong> package.`
+
+        summaryDiv.style.display = "block";
+        document.getElementById("form").style.display = "none";
     }
 });
 
